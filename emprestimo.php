2e3e07d3a60d3e1fa0e745dda8dd723db56d046b
Daniel Júnior <117056796+Judoloko@users.noreply.github.com>
2023-03-15 20:45:48 -0300

    Update emprestimo.php

--- emprestimo.php
+++ emprestimo.php
@@ -41,7 +41,7 @@ $custo_efetivo_total = $custo_parcela_com_juros * $parcelas + $tarifa + $imposto
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Resultado do Simulador de Empréstimo</title>
-    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <main>
